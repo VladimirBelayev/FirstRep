50b5b014513b10757de5cccc3179a5baef7e6df8
Владимир <belaevx16@gmail.com>
2021-11-14 15:26:31 +0300

    Созданно решение Task 1 (11,12,13,пункт 3)

---
+++ Task 1-1/main1.cpp
@@ -0,0 +1,49 @@
+﻿#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <iostream>
+
+/**
+Многострочный коментарий
+*/
+using namespace std;
+
+/**
+*\brief функция вычисляет A
+*\param x параметр x
+*\return 0 в случае успеха
+*/
+double getA(const double x);
+
+/**
+*\brief функция вычисляет B
+*\param x параметр x
+*\param y параметр y
+*\return 0 в случае успеха
+*/
+double getB(const double x, const double y);
+
+/**
+*\brief функция выводит результат вычислений на экран и исходные данные
+*\return 0 в случае успеха
+*/
+int main() {
+	const double x=0.335;
+	const double y=0.025; 
+
+	const double A = getA(x);
+	const double B = getB(x,y);
+
+	cout << "x=" << x << " y=" << y << " A=" << A << "  B=" << B;
+	return 0;
+}
+
+double  getA(const double x)
+{
+	return (1 + x +((pow(x, 2)) / 2) + ((pow(x, 3)) / 3) + ((pow(x, 4)) / 4));
+}
+
+double getB(const double x,const double y)
+{
+	return x * (sin(pow(x, 3)) + pow(cos(y),2));
+}
+   
---
+++ Task 1-2/main2.cpp
@@ -0,0 +1,50 @@
+﻿#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <iostream>
+
+/**
+Многострочный коментарий
+*/
+using namespace std;
+
+/**
+*\brief функция вычисляет объём шара
+*\param R параметр R
+*\param M_PI число пи
+*\return 0 в случае успеха
+*/
+double getV(double R);
+
+/**
+*\brief функция вычисляет площадь поверхности шара
+*\param R параметр R
+*\param M_PI число пи
+*\return 0 в случае успеха
+*/
+double getS(double R);
+
+/**
+*\brief функция выводит результат вычислений на экран
+*\return 0 в случае успеха
+*/
+int main()
+{
+    double R;
+    cout << "Введите радиус шара\n";
+    cin >> R;
+    const double V = getV(R);
+    const double S = getS(R);
+
+    cout << "S==" << S << "  V==" << V;
+    return 0;
+}
+
+double getV(double R)
+{
+    return 4 * ((M_PI * pow(R, 3) / 3));
+}
+
+double getS(double R)
+{
+    return 4 * (M_PI * pow(R, 2));
+}
