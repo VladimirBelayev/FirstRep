77c8cfa9f82dc1e3890b95a4568007de887c03f1
Владимир <belaevx16@gmail.com>
2022-01-20 09:48:38 +0300

    3.2

---
+++ Task  3-2/Source.cpp
@@ -0,0 +1,42 @@
+﻿﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * \brief Нахождение суммы n членов последовательности.
+ * \param k - номер члена последовательности.
+ * \param n - количество складываемых членов.
+ * \param nk - член последовательности.
+ * \param sum - искомая сумма
+ */
+int getSum(int k, int n, int nk, int sum);
+
+/**
+ * \brief Точка входа в программу.
+ * \return 0 в случае успеха.
+ */
+int main()
+{
+	int k;
+	k = 0;
+	int n;
+	cout << "Сколько членов последовательности сложить?\n";
+	cin >> n;
+	if (n < 2) {
+		return 0;
+	}
+	double nk = 1;
+	double sum = getSum(k, n, nk, sum);
+	cout << "Сумма = " << sum;
+	return 0;
+}
+
+int getSum(int k, int n, int nk, int sum) {
+	double nk = 1;
+	for (k = 0; k < n; k++)
+	{
+		nk = (nk * (-1)) / (k + 1);
+		sum += nk;
+	}
+}
